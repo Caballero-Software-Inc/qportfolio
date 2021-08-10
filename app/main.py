13f5a1859f16f014a37983696a309821d82eacb2
José Manuel Rodriguez Caballero <josephcmac@gmail.com>
2021-08-10 07:09:32 -0400

    update

--- app/main.py
+++ app/main.py
@@ -133,4 +133,3 @@ def main():
     return options[request.get_json()['selection']](request.get_json()['input'])
 
 
-app.run(port = os.environ.get('PORT', 5000))
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from app.main import  app 
+import os
+if __name__ == "__main__":
+    app.run(port = os.environ.get('PORT', 5000))
